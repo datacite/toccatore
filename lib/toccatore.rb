19723ce1f1774c2fbb4a3f09568117486623945f
Kristian Garza <kj.garza@gmail.com>
2018-05-04 18:15:34 +0200

    readd usage

--- lib/toccatore.rb
+++ lib/toccatore.rb
@@ -1,6 +1,6 @@
 require "toccatore/orcid_update"
 require "toccatore/datacite_related"
+require "toccatore/queue"
+require "toccatore/usage_update"
 require "toccatore/cli"
-# require "toccatore/queue"
 require "toccatore/version"
-# require "toccatore/usage_update"
